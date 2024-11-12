0a58f8c9bce6ee6f01614adf801211d8e796e3a3
Paul Thurmond <paulthurmond@gmail.com>
2024-11-11 19:59:47 -0600

    Completed D1Q2

--- 01/part2.js
+++ 01/part2.js
@@ -1,21 +1,33 @@
 const fs = require('fs');
-const data = fs.readFileSync('data1.txt', 'utf8');
+const data = fs.readFileSync('data2.txt', 'utf8');
 // const lines = data.split('\n');
 // console.log(lines);
 
-console.log(data);
+let scores = {
+  xx: 0,
+  Ax: 0,
+  Bx: 1,
+  Cx: 3,
+  Dx: 5,
+  AA: 2,
+  AB: 3,
+  AC: 5,
+  AD: 7,
+  BB: 4,
+  BC: 6,
+  BD: 8,
+  CC: 8,
+  CD: 10,
+  DD: 12,
+};
 
 let potions = 0;
 
-for (const char of data) {
-  console.log(char);
-  if (char === 'B') {
-    potions = potions + 1;
-  }
-  if (char === 'C') {
-    potions = potions + 3;
-  }
-  console.log(potions);
+for (let i = 0; i < data.length; i = i + 2) {
+  let pair = [data[i], data[i + 1]].sort().join('');
+  console.log(pair);
+  console.log(scores[pair]);
+  potions += scores[pair];
 }
 
 console.log(`Total potions needed: ${potions}`);
